ce8b50ba8d844a8bd4b0db77a46594268cdfe252
rzs <rasmus.norgaard@gmail.com>
2023-03-21 20:10:40 +0100

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true, 
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'quotes': ['error', 'single'],
+    // we want to force semicolons
+    'semi': ['error', 'always'],
+    // we use 2 spaces to indent our code
+    'indent': ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error']
+  }
+};
